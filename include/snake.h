b36209772828cc33cd23c54e439285a1d62c71f5
Vladislav <helloruby@localhost.localdomain>
2025-06-30 20:34:54 +0800

    added base movement

--- include/snake.h
+++ include/snake.h
@@ -1,22 +1,33 @@
 #include <ncurses.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-
-typedef struct Node
-{
-    unsigned int x;
-    unsigned int y;
-    struct Node* next;
+typedef struct Node {
+  unsigned int x;
+  unsigned int y;
+  struct Node *next;
 } Node;
 
-typedef struct DefaultValues
-{
-    unsigned int defaultSnakeX;
-    unsigned int defaultSnakeY;
+typedef struct DefaultValues {
+  unsigned int defaultSnakeX;
+  unsigned int defaultSnakeY;
+  chtype snakeSymbol;
 } DefaultValues;
+typedef struct Field {
+  unsigned int x_max;
+  unsigned int y_max;
+} Field;
 
-Node* create_node(unsigned int x, unsigned int y);
-void insert_at_head(Node** head, Node* node);
-void delete_list(Node* head);
-void remove_node(Node** head, Node* node_to_remove);
+typedef enum { LEFT, RIGHT, UP, DOWN } Directions;
 
-Node* init_snake(DefaultValues _default);
+Node *create_node(unsigned int x, unsigned int y);
+void insert_at_head(Node **head, Node *node);
+void delete_list(Node *head);
+void remove_node(Node **head, Node *node_to_remove);
+
+Node *init_snake(DefaultValues _default);
+Directions get_direction(Directions *dir, Directions prev_dir);
+void move_snake(Directions dir, Node **snake_head);
+void display(Node *snake);
+bool is_beyond_border(Node *snake_head, Field field);
--- src/main.c
+++ src/main.c
@@ -1,13 +1,46 @@
 #include "../include/snake.h"
 
-int main(int argc, char* argv[])
-{
-    initscr();
-  
-    DefaultValues dv = {10, 10};
-    Node* snake = init_snake(dv);
+[[maybe_unused]] static Node *snake = NULL;
 
-    getch();
-    endwin();
-    return 0;
+static void signal_of_immediate_exit(int sig) {
+  delete_list(snake);
+  endwin();
+  exit(1);
+}
+
+int main(int argc, char *argv[]) {
+  initscr();
+
+  noecho();
+  cbreak();
+  curs_set(0);
+  keypad(stdscr, true);
+  nodelay(stdscr, true);
+
+  signal(SIGINT, signal_of_immediate_exit);
+
+  unsigned int x_max, y_max;
+  getmaxyx(stdscr, y_max, x_max);
+  Field field = {x_max, y_max};
+
+  DefaultValues dv = {10, 10};
+  snake = init_snake(dv);
+  [[maybe_unused]] Node *tmp_ptr = snake;
+
+  Directions dir = RIGHT;
+  Directions prev_dir = dir;
+
+  timeout(100);
+  while (dir != -1) {
+    clear();
+    display(snake);
+
+    prev_dir = get_direction(&dir, prev_dir);
+    move_snake(dir, &snake);
+    refresh();
+  }
+  delete_list(snake);
+
+  endwin();
+  return 0;
 }
